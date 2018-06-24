74ab5134ca563fc49ab18dfb460f3b89088816b2
OmarSalahMahmoud <omar2641995@gmail.com>
2018-06-25 01:06:51 +0200

    alasle

---
+++ mapApp/app/src/main/java/com/example/android/mapapp/MapsActivity.java
@@ -0,0 +1,56 @@
+package com.example.android.mapapp;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng cairouni = new LatLng(30.022756, 31.207502);
+        mMap.addMarker(new MarkerOptions().position(cairouni).title("Cairo University"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(cairouni));
+        LatLng Kasr3ene = new LatLng(30.031968, 31.228630);
+        mMap.addMarker(new MarkerOptions().position(Kasr3ene).title("Kasr 3AYNE"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Kasr3ene));
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.setMyLocationEnabled(true);
+    }
+}
